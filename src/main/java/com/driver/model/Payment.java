86d1f3429b78f073c87f49a29b831a66924e9ecc
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-01-28 20:01:17 +0530

    first commit

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -23,7 +23,7 @@ public class Payment {
         this.id = id;
     }
 
-    public boolean isPaymentCompleted() {
+    public boolean getPaymentCompleted() {
         return paymentCompleted;
     }
 
